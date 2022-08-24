58573719dc1bbd55528ed9537f656c132599f643
Pedro <pedroviniciusgonzaga@hotmail.com>
2022-08-24 12:46:06 -0300

    Adicionando PUT e DELETE

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from enum import unique
+import json
 from operator import methodcaller
 from flask import Flask, jsonify, request, make_response # Para fazer o sistema web
 from flask_sqlalchemy import SQLAlchemy #Para acessar o banco de dados
@@ -57,23 +58,57 @@ class VideosSchema(SQLAlchemySchema):
 #Fazendo página index para retornar todos os vídeos
 @app.route('/videos', methods=['GET'])
 def index():
-    getVideos = Videos.query.all()
-    videosSchema = VideosSchema(many=True)
-    videos = videosSchema.dump(getVideos)
+    getVideos = Videos.query.all() #Buscando todos os itens da tabela Vídeos
+    videosSchema = VideosSchema(many=True) #Instanciando videosSchema como objeto da classe VideosSchema
+    videos = videosSchema.dump(getVideos) 
     return make_response(jsonify({"Vídeos":videos}))
 
+#Método GET para cada vídeo por id
+@app.route('/videos/<id>', methods=['GET'])
+def getVideoById(id):
+    getVideo = Videos.query.get(id)
+    videosSchema = VideosSchema()
+    video = videosSchema.dump(getVideo)
+    return make_response(jsonify({"Vídeo": video}))
 
-#Fazendo método post para adicionar á página index
+
+#Fazendo método post para adicionar á página videos
 @app.route('/videos', methods=['POST'])
 def createVideo():
     data = request.get_json() #Pegando formato JSON
     videosSchema = VideosSchema() #Instanciando novo objeto na classe VideosSchema
     videos = videosSchema.load(data) #Carregando dados na variável videos
-    result = videosSchema.dump(product.create()) #Criando retorno na variável result
+    result = videosSchema.dump(videos.create()) #Criando retorno na variável result
     return make_response(jsonify({"Vídeos":result}), 200) #Retornando a variável result como JSON
 
 
+#Fazendo método PUT para atualizar a página videos
+@app.route('/videos/<id>', methods=['PUT'])
+def atualizarVideoById(id):
+    data = request.get_json()
+    getVideos = Videos.query.get(id)
+    if data.get('titulo'):
+        getVideos.titulo = data['titulo']
+    if data.get('descricao'):
+        getVideos.descricao = data['descricao']
+    if data.get('url'):
+        getVideos.url = data['url']
+    
+    db.session.add(getVideos)
+    db.session.commit()
+    VideosSchema = VideosSchema(only=['id', 'titulo', 'descricao', 'url'])
+    videos = videosSchema.dump(getVideos)
+    return make_response(jsonify({'Vídeos': videos}))
+
+
+#Método DELETE
+@app.route('/videos/<id>', methods=['DELETE'])
+def deleteVideoById(id):
+    getVideos = db.query.get(id)
+    db.session.delete(getVideos)
+    db.session.commit()
+    return make_response('', 204)
 
 
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
