0b4010d7d5fee2b73fa073593a42cc86a5f66a66
Pedro <pedroviniciusgonzaga@hotmail.com>
2022-08-24 12:15:28 -0300

    Criando index com métodos GET e POST

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from enum import unique
+from operator import methodcaller
 from flask import Flask, jsonify, request, make_response # Para fazer o sistema web
 from flask_sqlalchemy import SQLAlchemy #Para acessar o banco de dados
 from marshmallow_sqlalchemy import SQLAlchemySchema #Serializar o banco
@@ -40,4 +41,39 @@ class Videos(db.Model):
 
 db.create_all()
     
+#Mapear as variáveis para os atributos da classe Videos
 
+class VideosSchema(SQLAlchemySchema):
+    class Meta(SQLAlchemySchema.Meta): #Definir model para relacionar com o schema, para retornar JSON
+        model = Videos
+        sqla_session = db.session
+    id = fields.Number(dump_only=True)
+    titulo = fields.String(required=True)
+    descricao = fields.String(required=True)
+    url = fields.String(required=True)
+
+'''Rotas e métodos Flask'''
+
+#Fazendo página index para retornar todos os vídeos
+@app.route('/videos', methods=['GET'])
+def index():
+    getVideos = Videos.query.all()
+    videosSchema = VideosSchema(many=True)
+    videos = videosSchema.dump(getVideos)
+    return make_response(jsonify({"Vídeos":videos}))
+
+
+#Fazendo método post para adicionar á página index
+@app.route('/videos', methods=['POST'])
+def createVideo():
+    data = request.get_json() #Pegando formato JSON
+    videosSchema = VideosSchema() #Instanciando novo objeto na classe VideosSchema
+    videos = videosSchema.load(data) #Carregando dados na variável videos
+    result = videosSchema.dump(product.create()) #Criando retorno na variável result
+    return make_response(jsonify({"Vídeos":result}), 200) #Retornando a variável result como JSON
+
+
+
+
+if __name__=="__main__":
+    app.run()
